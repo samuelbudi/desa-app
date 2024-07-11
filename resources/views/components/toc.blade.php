85bdd72c54eca2a0fb7e8ed30b3260c479b4a737
samuelbudi <samuel.budi.satrio@mail.ugm.ac.id>
2024-07-11 18:30:40 +0700

    update: frontend finish except form

---
+++ resources/views/components/toc.blade.php
@@ -0,0 +1,12 @@
+<div href="#" class="block p-6 bg-white border border-gray-200 rounded-lg shadow ">
+    <h1 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Daftar Isi</h1>
+    <div data-toc style="color:#1A56DB; max-height: 750px;" class="toc"></div>
+    <script>
+        tableOfContents('[data-content]', '[data-toc]', {
+            levels: 'h1, h2, h3, h4, h5, h6', // The heading levels to generate a table of contents from
+            heading: '', // The heading text for the table of contents list
+            headingLevel: 'h2', // The level to use for the heading for the table of contents list
+            listType: 'ul' // The list type to use for the table of contents
+        });
+    </script>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,24 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/umkm', function (){
+Route::get('/umkm', function () {
     return view('umkm');
 });
 
-Route::get('/wisata', function (){
+Route::get('/wisata', function () {
     return view('wisata');
 });
 
-Route::get('/wilayah', function (){
+Route::get('/wilayah', function () {
     return view('wilayah');
 });
 
-Route::get('/tentangkami', function (){
+Route::get('/tentangkami', function () {
     return view('tentangkami');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/saran', function () {
+    return view('kirimsaran');
 });
