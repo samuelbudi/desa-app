e756a04e8b04df58e3e6229edae1ffffc373d6da
samuelbudi <samuel.budi.satrio@mail.ugm.ac.id>
2024-07-11 10:39:35 +0700

    update: -

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/umkm', function () {
+    return view('umkm');
+});
+
+Route::get('/wisata', function () {
+    return view('wisata');
+});
+
+Route::get('/tentang-kami', function () {
+    return view('tentangkami');
+});
+
+Route::get('/wilayah', function () {
+    return view('wilayah');
+});
